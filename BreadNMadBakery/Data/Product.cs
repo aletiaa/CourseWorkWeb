1b856484f66e0b5afb87a4bb34b2ea7d7a007b54
aletiaa <alina.seikauskaite3@gmail.com>
2023-06-01 18:41:35 +0300

    added no-image

--- BreadNMadBakery/Data/Product.cs
+++ BreadNMadBakery/Data/Product.cs
@@ -1,5 +1,7 @@
 public class Product 
 {
+    private string _imageName;
+
     public Product() 
     {
         this.OrdersProducts = new List<OrderProduct>();
@@ -8,7 +10,24 @@ public class Product
     public int ID { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
-    public string ImageName {get; set; }
+    
+    public string? ImageName 
+    {
+        get
+        {  
+            if (_imageName == null) 
+            {
+                return "no-image.jpg";
+            }
+
+            return _imageName;
+        }
+
+        set
+        {
+            _imageName = value;
+        } 
+    }
 
     public List<OrderProduct> OrdersProducts { get; set; }
 }
