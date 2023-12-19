571b071f2e26633ebac46411f6d568dad8a4cdc4
aletiaa <alina.seikauskaite3@gmail.com>
2023-12-19 22:30:27 +0000

    header test

--- SeleniumTesting/Main/MainTests.cs
+++ SeleniumTesting/Main/MainTests.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -20,5 +21,24 @@ public class MainTests
         Assert.AreEqual("- BreadNMadBakery", pageTitle);
 
         driver.Quit();
+    }
+
+    [TestMethod]
+    public void MainHeaderText()
+    {
+        //arrange
+        var driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://localhost:5253");
+        var headerElement = driver.FindElement(By.TagName("header"));
+        var paragraphElement = headerElement.FindElement(By.TagName("p"));
+
+        //act
+        var className = paragraphElement.GetAttribute("class");
+        var paragraphText = paragraphElement.Text;
+
+        //assert
+        Assert.AreEqual("company-name", className);
+        Assert.AreEqual("BREAD'N'MAD", paragraphText);
+      
     }
 }
